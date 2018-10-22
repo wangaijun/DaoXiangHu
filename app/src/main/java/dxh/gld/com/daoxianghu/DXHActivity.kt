51e3df2fe429750b4f1636b2bf71ff5a85735503
hw_wangaijun@163.com <waj67011>
2018-10-23 06:01:24 +0800

    增加稻香湖地图

---
+++ app/src/main/java/dxh/gld/com/daoxianghu/DXHActivity.kt
@@ -0,0 +1,12 @@
+package dxh.gld.com.daoxianghu
+
+import android.app.Activity
+import android.os.Bundle
+
+class DXHActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dxh)
+    }
+}
