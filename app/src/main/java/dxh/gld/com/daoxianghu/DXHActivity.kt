a83bf6493b8c9efbc0df0f8e9b7844115816bbda
wangaijun <hw_wangaijun@163.com>
2018-10-24 15:21:58 +0800

    添加考试项按钮

--- app/src/main/java/dxh/gld/com/daoxianghu/DXHActivity.kt
+++ app/src/main/java/dxh/gld/com/daoxianghu/DXHActivity.kt
@@ -1,8 +1,11 @@
 package dxh.gld.com.daoxianghu
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_dxh.*
 
 class DXHActivity : Activity() {
@@ -27,6 +30,30 @@ class DXHActivity : Activity() {
                 }
             }
         }.start()
+    }
+
+    fun onClick(view: View) {
+        when(view.id){
+            R.id.tv1->toast("打灯，挂挡，松手刹")
+            R.id.tv2->toast("踩刹车")
+            R.id.tv3->toast("踩刹车，30以内")
+            R.id.tv4->toast("打灯，踩刹车")
+            R.id.tv5->toast("踩刹车")
+            R.id.tv6->toast("直线，35左右")
+            R.id.tv7->toast("打灯，踩刹车，看直行车")
+            R.id.tv8->toast("打右灯")
+            R.id.tv9->toast("过指示牌30米，黄漆处，踩刹车，30以内")
+            R.id.tv10->toast("打右灯，踩刹车，减至两档")
+            R.id.tv11->toast("打左灯，直行，打右灯")
+            R.id.tv12->toast("踩刹车")
+            R.id.tv13->toast("打右灯，踩刹车，对白块，停车，拉手刹，挂空挡，松离合、脚刹，开门，关门，考官签字")
+        }
+    }
+
+    private fun toast(s: String) {
+        AlertDialog.Builder(this).setMessage(s)
+            .setPositiveButton("确定",null)
+            .create().show()
     }
 
     companion object {
